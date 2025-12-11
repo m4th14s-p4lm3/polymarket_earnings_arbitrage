8ff344976901fed88a4c4a6c98bcd2fc28ad72b0
Mathias Palme <mathias.palme@seznam.cz>
2025-12-11 22:42:11 +0000

    Added price tracking before oracle

--- order.py
+++ order.py
@@ -69,6 +69,11 @@ class EarningsMarket:
         print("ALERT TRIGGERED!!!!")
         logger.info(f"Trigger sent to slug: {self.slug}")
 
+        # get prices before oracle
+        price_yes = polymarket_api.DataFeed.get_market_price_for_token(self.outcome_addresses["Yes"])
+        price_no = polymarket_api.DataFeed.get_market_price_for_token(self.outcome_addresses["No"])
+        logger.info(f"Price after trigger - slug: {self.slug}, ticker: {self.ticker}, price_yes {price_yes}, price_no: {price_no}")
+
         timer_start = time.perf_counter()
         resolution = get_resolution(self.description, self.sec_url)
         self.oracle_time = time.perf_counter() - timer_start
@@ -79,6 +84,7 @@ class EarningsMarket:
             resolution = resolution[0].upper() + resolution[1:].lower()
             # resolution = "Yes"
             address = self.outcome_addresses[resolution]
+            # get pricee after oracle
             price = polymarket_api.DataFeed.get_market_price_for_token(address)
             telegram_bot.send_message(f"slug: {self.slug}, ticker: {self.ticker}, resolution: {resolution}, price: {price}, oracle time: {self.oracle_time}")
             logger.info(f"slug: {self.slug}, ticker: {self.ticker}, resolution: {resolution}, price: {price}, oracle time: {self.oracle_time}")
@@ -101,20 +107,46 @@ if __name__ == "__main__":
     # kfy_url = "https://polymarket.com/event/kfy-quarterly-earnings-nongaap-eps-12-04-2025-1pt31"
     # kfy_em = EarningsMarket(kfy_url, edgar_sentinel)
 
+
+
+
+    # 10/12/2025
     orcl_url = "https://polymarket.com/event/orcl-quarterly-earnings-nongaap-eps-12-08-2025-1pt64"
 
-    pcb_url = "https://polymarket.com/event/cpb-quarterly-earnings-nongaap-eps-12-09-2025-0pt73?tid=1765226600660"
-    azo_url = "https://polymarket.com/event/azo-quarterly-earnings-gaap-eps-12-09-2025-32pt58?tid=1765226611220"
-    dbi_url = "https://polymarket.com/event/dbi-quarterly-earnings-nongaap-eps-12-09-2025-0pt16?tid=1765226624433"
-    aso_url = "https://polymarket.com/event/aso-quarterly-earnings-nongaap-eps-12-09-2025-1pt06?tid=1765226633644"
-    
-    
-    orcl_em = EarningsMarket(orcl_url, edgar_sentinel)
+    chwy_url = "https://polymarket.com/event/chwy-quarterly-earnings-nongaap-eps-12-10-2025-0pt3"
+    adbe_url = "https://polymarket.com/event/adbe-quarterly-earnings-nongaap-eps-12-10-2025-5pt4"
+    mnt_url = "https://polymarket.com/event/mtn-quarterly-earnings-gaap-eps-12-10-2025-neg5pt24"
 
-    pcb_em = EarningsMarket(pcb_url, edgar_sentinel)
-    azo_em = EarningsMarket(azo_url, edgar_sentinel)
-    dbi_em = EarningsMarket(dbi_url, edgar_sentinel)
-    aso_em = EarningsMarket(aso_url, edgar_sentinel)
+    orcl_em = EarningsMarket(orcl_url, edgar_sentinel)
+    
+    chwy_em = EarningsMarket(chwy_url, edgar_sentinel)
+    adbe_em = EarningsMarket(adbe_url, edgar_sentinel)
+    mnt_em = EarningsMarket(mnt_url, edgar_sentinel)
+
+    # 11/12/2025
+    cost_url = "https://polymarket.com/event/cost-quarterly-earnings-gaap-eps-12-11-2025-4pt28"
+    avgo_url = "https://polymarket.com/event/avgo-quarterly-earnings-nongaap-eps-12-11-2025-1pt87"
+    rh_url = "https://polymarket.com/event/rh-quarterly-earnings-nongaap-eps-12-11-2025-2pt16"
+    lulu_url = "https://polymarket.com/event/lulu-quarterly-earnings-gaap-eps-12-11-2025-2pt21"
+
+    cost_em = EarningsMarket(cost_url, edgar_sentinel)
+    avgo_em = EarningsMarket(avgo_url, edgar_sentinel)
+    rh_em = EarningsMarket(rh_url, edgar_sentinel)
+    lulu_em = EarningsMarket(lulu_url, edgar_sentinel)
+
+
+
+    # pcb_url = "https://polymarket.com/event/cpb-quarterly-earnings-nongaap-eps-12-09-2025-0pt73?tid=1765226600660"
+    # azo_url = "https://polymarket.com/event/azo-quarterly-earnings-gaap-eps-12-09-2025-32pt58?tid=1765226611220"
+    # dbi_url = "https://polymarket.com/event/dbi-quarterly-earnings-nongaap-eps-12-09-2025-0pt16?tid=1765226624433"
+    # aso_url = "https://polymarket.com/event/aso-quarterly-earnings-nongaap-eps-12-09-2025-1pt06?tid=1765226633644"
+    
+    
+
+    # pcb_em = EarningsMarket(pcb_url, edgar_sentinel)
+    # azo_em = EarningsMarket(azo_url, edgar_sentinel)
+    # dbi_em = EarningsMarket(dbi_url, edgar_sentinel)
+    # aso_em = EarningsMarket(aso_url, edgar_sentinel)
 
 
     print("Market is runnning...")
